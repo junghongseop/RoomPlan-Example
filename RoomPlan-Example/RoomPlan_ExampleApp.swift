13b24bfc440f94bec38a0de9ae50cfbfd34b2d9f
정홍섭 <a01082372487@gmail.com>
2026-05-24 14:39:16 +0900

    Initial Commit

---
+++ RoomPlan-Example/RoomPlan_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomPlan_ExampleApp.swift
+//  RoomPlan-Example
+//
+//  Created by 정홍섭 on 5/24/26.
+//
+
+import SwiftUI
+
+@main
+struct RoomPlan_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
